fdd270bf400c68ed4b1a3bac68c4d10d4ca29181
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2024-03-15 22:45:29 +0530

    30 days Pandas

---
+++ LeetCode/30 Days of Pandas/nth_highest_sale.py
@@ -0,0 +1,84 @@
+# 177. Nth Highest Salary
+
+# Table: Employee
+
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | id          | int  |
+# | salary      | int  |
+# +-------------+------+
+# id is the primary key (column with unique values) for this table.
+# Each row of this table contains information about the salary of an employee.
+
+
+# Write a solution to find the nth highest salary from the Employee table. If there is no nth highest salary, return null.
+
+# The result format is in the following example.
+
+
+# Example 1:
+
+# Input:
+# Employee table:
+# +----+--------+
+# | id | salary |
+# +----+--------+
+# | 1  | 100    |
+# | 2  | 200    |
+# | 3  | 300    |
+# +----+--------+
+# n = 2
+# Output:
+# +------------------------+
+# | getNthHighestSalary(2) |
+# +------------------------+
+# | 200                    |
+# +------------------------+
+# Example 2:
+
+# Input:
+# Employee table:
+# +----+--------+
+# | id | salary |
+# +----+--------+
+# | 1  | 100    |
+# +----+--------+
+# n = 2
+# Output:
+# +------------------------+
+# | getNthHighestSalary(2) |
+# +------------------------+
+# | null                   |
+# +------------------------+
+
+
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    # Sort the dataframe by the 'salary' column in descending order
+    sorted_df = employee.sort_values(by='salary', ascending=False)
+    
+    # Reset the index after sorting
+    sorted_df.reset_index(drop=True, inplace=True)
+    
+    # Check if there are at least N unique salaries
+    if N <= len(sorted_df):
+        # Select the Nth highest salary using iloc
+        nth_highest_salary = sorted_df['salary'].iloc[N - 1]
+        return pd.DataFrame({f'getNthHighestSalary({N})': [nth_highest_salary]})
+    else:
+        # If there are fewer than N unique salaries, return null
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
+    
+
+
+employee = pd.DataFrame({
+    'id': [1, 2, 3],
+    'salary': [100, 200, 300]
+})
+
+N = 2
+
+output = nth_highest_salary(employee, N)
+print(output)
